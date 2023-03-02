edb854c7a1eed5f6dabd658adead76cc389b7235
lovretomic <tomiclovre05@gmail.com>
2023-03-02 10:05:22 +0100

    Added code fetching and styles

---
+++ main.js
@@ -0,0 +1,22 @@
+const baseUrl = 'https://homework-server1.onrender.com';
+const key = 'lovretomic';
+
+const codeNums = document.querySelector('.code__nums');
+const codeContent = document.querySelector('.code__content');
+
+fetch(`${baseUrl}/code`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      key
+    },
+  })
+    .then((response) => response.json())
+    .then((response) => response.code.split('\n'))
+    .then((response) => {
+      for(let i = 0; i < response.length; i++) {
+        codeContent.innerHTML += `<p class="code__content-line">${response[i]}</p>`;
+        codeNums.innerHTML += `<div class="code__nums-num">${i}</div>`;
+      }
+    })
+    .catch((err) => console.log(err));
