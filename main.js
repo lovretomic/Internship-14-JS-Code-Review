e274ec2d5892b7470baf125e4957d703b2f623ea
lovretomic <tomiclovre05@gmail.com>
2023-03-04 19:36:19 +0100

    Added liking system

--- main.js
+++ main.js
@@ -18,62 +18,69 @@ const addNoteMenu = document.querySelector('.code__add');
 const lineHeight = 25;
 
 fetch(`${baseUrl}/code`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      key
-    },
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json',
+    key
+  },
+})
+  .then((response) => response.json())
+  .then((response) => response.code.split('\n'))
+  .then((response) => {
+    for(let i = 0; i < response.length; i++) {
+      codeContent.innerHTML += `<p class="code__content-line">${response[i]}</p>`;
+      codeNumsBar.innerHTML += `<div class="code__nums-num">${i + 1}</div>`;
+    }
   })
-    .then((response) => response.json())
-    .then((response) => response.code.split('\n'))
-    .then((response) => {
-      for(let i = 0; i < response.length; i++) {
-        codeContent.innerHTML += `<p class="code__content-line">${response[i]}</p>`;
-        codeNumsBar.innerHTML += `<div class="code__nums-num">${i + 1}</div>`;
-      }
-    })
-    .then(() => {
-      codeNums = document.querySelectorAll('.code__nums-num');
-      for(let i = 0; i < codeNums.length; i++) {
-        codeNums[i].addEventListener('click', () => {
-          if (addNoteMenuStatus.index !== -1) codeNums[addNoteMenuStatus.index].classList.remove('selected');
-          if (addNoteMenuStatus.opened === false) toggleAddNoteMenu(i);
-          else {addNoteMenuStatus.index = i; refreshAddNoteMenu();}
+  .then(() => {
+    codeNums = document.querySelectorAll('.code__nums-num');
+    for(let i = 0; i < codeNums.length; i++) {
+      codeNums[i].addEventListener('click', () => {
+        if (addNoteMenuStatus.index !== -1) codeNums[addNoteMenuStatus.index].classList.remove('selected');
+        if (addNoteMenuStatus.opened === false) toggleAddNoteMenu(i);
+        else {addNoteMenuStatus.index = i; refreshAddNoteMenu();}
 
-          addNoteMenu.style.top = `${5 + i * 25}px`;
-          codeNums[i].classList.add('selected');
+        addNoteMenu.style.top = `${5 + i * 25}px`;
+        codeNums[i].classList.add('selected');
 
-          const noteSection = document.querySelector('.code__add-note');
-          const addNoteButton = document.querySelector('.code__add-buttons-button:nth-of-type(2)');
-          const commentSection = document.querySelector('.code__add-comment');
-          const addCommentButton = document.querySelector('.code__add-buttons-button:nth-of-type(1)');
+        const noteSection = document.querySelector('.code__add-note');
+        const addNoteButton = document.querySelector('.code__add-buttons-button:nth-of-type(2)');
+        const commentSection = document.querySelector('.code__add-comment');
+        const addCommentButton = document.querySelector('.code__add-buttons-button:nth-of-type(1)');
 
-          noteSection.style.display = 'none';
-          commentSection.style.display = 'none';
+        noteSection.style.display = 'none';
+        commentSection.style.display = 'none';
 
-          const notesData = JSON.parse(localStorage.getItem(lsKey));
-          for (data of notesData) {
-            if (data.line == i) {
-              const noteSectionOutput = document.querySelector('.code__add-note > .code__add-output');
-              noteSection.style.display = 'flex';
-              noteSectionOutput.innerHTML = data.body;
-              addNoteButton.disabled = true;
-              break;
-            }
+        const notesData = JSON.parse(localStorage.getItem(lsKey));
+        for (data of notesData) {
+          if (data.line == i) {
+            if (data.isLiked) noteSection.classList.add('liked');
+            else noteSection.classList.remove('liked');
+            const noteSectionOutput = document.querySelector('.code__add-note > .code__add-output');
+            noteSection.style.display = 'flex';
+            noteSectionOutput.innerHTML = data.body;
+            addNoteButton.disabled = true;
+            break;
           }
-          for (data of commentsData.comments) {
-            if (data.line == i) {
-              const commentSectionOutput = document.querySelector('.code__add-comment > .code__add-output');
-              commentSection.style.display = 'flex';
-              commentSectionOutput.innerHTML = data.body;
-              addCommentButton.disabled = true;
-            }
-          } 
-        })
-      }
-    })
-    .then(() => refresh())
-    .catch((err) => console.log(err));
+        }
+        for (data of commentsData.comments) {
+          if (data.line == i) {
+            if (data.isLiked) commentSection.classList.add('liked');
+            else noteSection.classList.remove('liked');
+            const commentSectionOutput = document.querySelector('.code__add-comment > .code__add-output');
+            commentSection.style.display = 'flex';
+            commentSectionOutput.innerHTML = data.text;
+            addCommentButton.disabled = true;
+            break;
+          }
+        }
+
+        
+      })
+    }
+  })
+  .then(() => refresh())
+  .catch((err) => console.log(err));
 
 window.addEventListener('click', (e) => {  
   if (!(addNoteMenu.contains(e.target) || codeNumsBar.contains(e.target))){
@@ -90,7 +97,7 @@ class Note {
   post() {
     if (this.type === 'note') {
       let allNotes = JSON.parse(localStorage.getItem(lsKey));
-      allNotes.push({line: this.line, body: this.body});
+      allNotes.push({line: this.line, body: this.body, isLiked: false});
       localStorage.setItem(lsKey, JSON.stringify(allNotes));
     }
     else {
@@ -144,6 +151,22 @@ function removeNote() {
   toggleAddNoteMenu();
 }
 
+function likeNote() {
+  if (addNoteMenuStatus.index === -1) return;
+  let lsData = JSON.parse(localStorage.getItem(lsKey));
+  for (data of lsData)
+    if (data.line === addNoteMenuStatus.index) {
+      if (!data.isLiked)
+        data.isLiked = true;
+      else
+        data.isLiked = false;
+      break;
+    }
+  localStorage.setItem(lsKey, JSON.stringify(lsData));
+  refresh();
+  toggleAddNoteMenu();
+}
+
 function addComment() {
   if (addNoteMenuStatus.index < 0) { console.error('No line selected.'); return; }
   const input = document.querySelector('.code__add-input').innerHTML;
@@ -158,10 +181,64 @@ function addComment() {
   document.querySelector('.code__add-input').innerHTML = '';
 }
 
-function refresh() {
-  for (line of codeNums)
-    line.classList.remove('note');
+function removeComment() {
+  if (addNoteMenuStatus.index === -1) return;
 
+  let removeId;
+  for (data of commentsData.comments) {
+    if (data.line === addNoteMenuStatus.index) {
+      removeId = data.id;
+    }
+  }
+
+  fetch(`${baseUrl}/remove/${removeId}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+      key,
+    },
+  })
+  .then(response => console.log(response))
+
+  codeNums[addNoteMenuStatus.index].classList.remove('comment');
+  refresh();
+  toggleAddNoteMenu();
+}
+
+function likeComment() {
+  console.log('likecomment');
+  if (addNoteMenuStatus.index === -1) return;
+  
+  let likeId;
+  let isAlreadyLiked;
+  for (data of commentsData.comments) {
+    if (data.line === addNoteMenuStatus.index) {
+      likeId = data.id;
+      isAlreadyLiked = data.isLiked;
+    }
+  }
+
+  fetch(`${baseUrl}/update-is-liked/${likeId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      key,
+    },
+    body: JSON.stringify({
+      isLiked: !isAlreadyLiked
+    })
+  })
+  .then((response) => console.log(response))
+  refresh();
+  toggleAddNoteMenu();
+}
+
+function refresh() {
+  for (line of codeNums) {
+    line.classList.remove('note');
+    line.classList.remove('comment');
+  }
+    
   const notesData = JSON.parse(localStorage.getItem(lsKey));
   fetch(`${baseUrl}/comments`, {
     method: 'GET',
@@ -172,7 +249,6 @@ function refresh() {
   })
   .then((response) => response.json())
   .then((json) => commentsData = json)
-  .then(() => refresh())
 
   for (data of notesData) {
     codeNums[data.line].classList.add('note');
